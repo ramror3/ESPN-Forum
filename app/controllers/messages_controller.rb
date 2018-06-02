f67a698b7db1b6573ea6cdbfe7f9d26075ef7de4
ramror <ramesh.ramror@gmail.com>
2018-06-02 20:01:55 +0530

    Worked on Delete and update

--- app/controllers/messages_controller.rb
+++ app/controllers/messages_controller.rb
@@ -1,11 +1,13 @@
 class MessagesController < ApplicationController
-  
+
+  before_action :find_message, only: [:show, :edit, :update, :destroy]
+
   def index
     @messages = Message.all.order("created_at DESC")
   end
 
   def show
-    @message = Message.find(params[:id])
+
   end
   
   def new
@@ -21,8 +23,29 @@ class MessagesController < ApplicationController
     end
   end
 
+  def edit
+  end
+
+  def update
+    if @message.update(message_params)
+      redirect_to message_path
+    else
+      render 'edit'
+    end
+  end
+
+  def destroy
+    if @message.destroy
+      redirect_to root_path
+    end
+  end
+
   private
   def message_params
     params.require(:message).permit(:title, :description)
+  end
+
+  def find_message
+    @message = Message.find(params[:id])
   end
 end
