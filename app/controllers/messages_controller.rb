18263a9e40c38798db22fc3e2015a958ecf2245e
ramror <ramesh.ramror@gmail.com>
2018-06-02 13:40:46 +0530

    Add index and show page

--- app/controllers/messages_controller.rb
+++ app/controllers/messages_controller.rb
@@ -1,8 +1,13 @@
 class MessagesController < ApplicationController
   
   def index
+    @messages = Message.all.order("created_at DESC")
   end
 
+  def show
+    @message = Message.find(params[:id])
+  end
+  
   def new
     @message = Message.new
   end
